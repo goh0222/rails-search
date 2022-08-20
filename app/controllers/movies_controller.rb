48c4f436508d8f128c95c95d952802a12545f348
goh <goh022022@gmail.com>
2022-08-20 10:21:12 +0800

    use PgSearch to retrieve movie results

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,20 @@
 class MoviesController < ApplicationController
   def index
     if params[:query].present?
-      sql_query = <<~SQL
-        movies.title @@ :query
-        OR movies.synopsis @@ :query
-        OR directors.first_name @@ :query
-        OR directors.last_name @@ :query
-      SQL
-      @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+      # sql_query = <<~SQL
+      #   movies.title @@ :query
+      #   OR movies.synopsis @@ :query
+      #   OR directors.first_name @@ :query
+      #   OR directors.last_name @@ :query
+      # SQL
+      # @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+
+      # use PgSearch to retrieve movie results
+      results = PgSearch.multisearch(params[:query])
+      @movies = []
+      results.each do |result|
+        @movies << Movie.find(result.searchable_id)
+      end
     else
       @movies = Movie.all
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,14 +1,6 @@
 class Movie < ApplicationRecord
-  include PgSearch::Model
-
   belongs_to :director
 
-  pg_search_scope :global_search,
-  against: [ :title, :synopsis ],
-  associated_against: {
-    director: [ :first_name, :last_name ]
-  },
-  using: {
-    tsearch: { prefix: true }
-  }
+  include PgSearch::Model
+  multisearchable against: [:title, :synopsis]
 end
