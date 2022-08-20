95dcc5927742e4c6d4aa823dd7ea5398d61d0cda
goh <goh022022@gmail.com>
2022-08-20 10:09:31 +0800

    Update pg_search_scope

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ApplicationRecord
+  include PgSearch::Model
+
   belongs_to :director
+
+  pg_search_scope :global_search,
+  against: [ :title, :synopsis ],
+  associated_against: {
+    director: [ :first_name, :last_name ]
+  },
+  using: {
+    tsearch: { prefix: true }
+  }
 end
